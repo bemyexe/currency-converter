9f9f373b09a0152c79073bf594e597da5e6be7d8
maxim <trifonov.max@mail.ru>
2024-05-26 11:31:33 +0300

    feature(styles): added reset css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CurrencyConverter from "./app";
 
+import "./styles/reset.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CurrencyConverter />
