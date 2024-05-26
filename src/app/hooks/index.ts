1a7608e9da9be3f2f8edc4b0de4dd57d1802b5b5
maxim <trifonov.max@mail.ru>
2024-05-26 12:27:25 +0300

    feautre(hook): useConversionRate

---
+++ src/app/hooks/index.ts
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
+
+interface useConversionRateParams {
+  baseCode: string;
+  targetCode: string;
+}
+interface useConversionRateResult {
+  conversionRate: number | null;
+  isLoading: boolean;
+  error: string;
+  clearError: () => void;
+}
+
+function useConversionRate({
+  baseCode,
+  targetCode,
+}: useConversionRateParams): useConversionRateResult {
+  const [conversionRate, setConversionRate] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const getConversionRate = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/${
+          import.meta.env.VITE_API_KEY
+        }/pair/${baseCode}/${targetCode}`
+      );
+      const data = await res.json();
+      setConversionRate(data.conversion_rate);
+    } catch {
+      setError("Something get wrong");
+    }
+  }, [setIsLoading, baseCode, targetCode, setError]);
+
+  const clearError = () => setError("");
+
+  useEffect(() => {
+    getConversionRate();
+  }, [getConversionRate]);
+
+  return { conversionRate, isLoading, error, clearError };
+}
+
+export default useConversionRate;
